Fixed issues with energy normalisation

kymatiomod/scattering2d/.ipynb_checkpoints/filter_bank-checkpoint.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.fft import fft2, ifft2
-from .utils import sqrt
+from scipy.special import factorial
+from .utils import sqrt, double_factorial
 
 def circular_harmonic_filter_bank(M, N, J, L, sigma_0, fourier=True):
     """
@@ -64,58 +65,39 @@ def circular_harmonic_2d(M, N, sigma, l, fourier=True):
             the 2l+1 wavelets of order (l , m) with -l <= m <= l.
             It is ifftshifted such that the origin is at the point [., 0, 0, 0]
     """
-    circ_harm = np.zeros((M, N), np.complex64)
     grid = np.fft.ifftshift(
         np.mgrid[-M // 2 : -M // 2 + M,
-                 -N // 2:-N // 2 + N].astype('float32'),
-        axes=(1,2))
-    _sigma = sigma
+                 -N // 2 : -N // 2 + N].astype('float32'),
+        axes=(1, 2)
+    )
 
     if fourier:
         grid[0] *= 2 * np.pi / M
         grid[1] *= 2 * np.pi / N
         _sigma = 1. / sigma
+    else:
+        _sigma = sigma
 
-    # r-coeffs for the scaling of the solution
     r_square = (grid ** 2).sum(0)
-    r_power_l = sqrt(r_square ** l)
+    r_power_l = np.power(r_square, l / 2)
 
-    gaussian = np.exp(-0.5 * r_square / _sigma**2).astype('complex64')
+    gaussian = np.exp(-0.5 * r_square / _sigma ** 2).astype(np.complex64)
 
     if l == 0:
-        if fourier:
-            return gaussian.reshape((1, M, N))
-        return gaussian.reshape((1, M, N)) / (
-                                          (2 * np.pi) * _sigma ** 2)
-
-    polynomial_gaussian = r_power_l * gaussian / _sigma ** l
-    # polynomial_gaussian = gaussian
-
-    # polar, azimuthal = get_3d_angles(grid)
-    theta = np.arctan2(grid[0], grid[1])
-
-    # for i_m, m in enumerate(range(-l, l + 1)):
-        #i.e. 1j amounts to the complex-coeff i, m is each candidate frequency, theta is all angles within the image/grid.
-    circ_harm = np.exp(1j * l * theta) * polynomial_gaussian
-
-    # if l % 2 == 0:
-    #     norm_factor = 1. / (2 * np.pi * np.sqrt(l + 0.5) * 
-    #                                         double_factorial(l + 1))
-    # else :
-    #     norm_factor = 1. / (2 ** (0.5 * ( l + 3)) * 
-    #                         np.sqrt(np.pi * (2 * l + 1)) * 
-    #                         factorial((l + 1) / 2))
-
-    norm_factor = 1 / (2 * np.pi)
-    
-    if fourier:
-        norm_factor *= (2 * np.pi) * (-1j) ** l
+        wavelet = gaussian
     else:
-        norm_factor /= _sigma ** 2
+        theta = np.arctan2(grid[0], grid[1])
+        angular = np.exp(1j * l * theta)
+        wavelet = r_power_l * angular * gaussian / (_sigma ** l)
 
-    circ_harm *= norm_factor
+    # Normalize to unit L2 norm
+    norm = np.sqrt(np.sum(np.abs(wavelet)**2))
 
-    return circ_harm
+    if fourier:
+        norm *= (M * N)
+    wavelet /= norm
+
+    return wavelet
     
 
 def filter_bank(M, N, J, L=8):
@@ -229,7 +211,7 @@ def gaussian_2d(M, N, sigma, fourier=True):
 
     gaussian = np.exp(-0.5 * (grid ** 2).sum(0) / _sigma ** 2)
     if not fourier:
-        gaussian /= (2 * np.pi) ** 1.5 * _sigma ** 3
+        gaussian /= (2 * np.pi) * _sigma ** 2
 
     return gaussian
     

kymatiomod/scattering2d/backend/tensorflow_backend.py
@@ -111,33 +111,7 @@ class TensorFlowBackend2D(TensorFlowBackend):
         out = tf.reduce_mean(y, axis=(1, 3))
         return out
         
-    @staticmethod
-    def modulus_rotation(x, module):
-        """Used for computing rotation invariant scattering transform coefficents.
 
-            Parameters
-            ----------
-            x : tensor
-                Size (batchsize, M, N, O).
-            module : tensor
-                Tensor that holds the overall sum.
-
-            Returns
-            -------
-            output : tensor
-                Tensor of the same size as input_array. It holds the output of
-                the operation::
-
-                $\\sqrt{\\sum_m (\\text{input}_\\text{array} \\star \\psi_{j,l,m})^2)}$
-
-                which is covariant to 3D translations and rotations.
-
-        """
-        if module is None:
-            module = tf.abs(x) ** 2
-        else:
-            module = module ** 2 + tf.abs(x) ** 2
-        return tf.sqrt(module)
 
     @staticmethod
     def compute_integrals(input_array, integral_powers):
@@ -190,8 +164,8 @@ class TensorFlowBackend2D(TensorFlowBackend):
         # if B.ndim != 3:
         #     raise RuntimeError('The dimension of the second input must be 3.')
 
-        Cr = tf.cast(tf.math.real(A) * np.real(B) - tf.math.imag(A) * np.imag(B), tf.complex64)
-        Ci = tf.cast(tf.math.real(A) * np.imag(B) + tf.math.imag(A) * np.real(B), tf.complex64)
+        Cr = tf.cast(tf.math.real(A) * tf.math.real(B) - tf.math.imag(A) * tf.math.imag(B), tf.complex64)
+        Ci = tf.cast(tf.math.real(A) * tf.math.imag(B) + tf.math.imag(A) * tf.math.real(B), tf.complex64)
 
         return Cr + 1.0j * Ci
 

kymatiomod/scattering2d/core/scattering2d.py
@@ -19,7 +19,7 @@ def harmonic_scattering2d(x, filters, rotation_covariant, L, J, max_order, backe
     ifft = backend.ifft
     cdgmm2d = backend.cdgmm2d
     modulus = backend.modulus
-    modulus_rotation = backend.modulus_rotation
+    # modulus_rotation = backend.modulus_rotation
 
     # out_S_0, out_S_1, out_S_2 = [], [], []
 
@@ -86,12 +86,11 @@ def harmonic_scattering2d(x, filters, rotation_covariant, L, J, max_order, backe
 
     
     S = stack(S)
+    
 
     # warnings.warn(str(S.shape)) 
     # out_S = stack((s_order_1, s_order_2), dim=-1)
     # S = stack(S)
-    # S = tf.reshape(S, ((S.shape[0], S.shape[1] // (L + 1), (L + 1))) + (S.shape[2:]))
-
 
     return S 
 

kymatiomod/scattering2d/frontend/tensorflow_frontend.py
@@ -136,9 +136,12 @@ class HarmonicScatteringTensorFlow2D(ScatteringTensorFlow, HarmonicScatteringBas
                              backend=self.backend, averaging=self.averaging)
 
             # print(S)
+            S = tf.reshape(S, tuple((tf.shape(S)[0], tf.shape(S)[1] // (L + 1), (L + 1))) + tuple(tf.shape(S)[2:]))
 
-            scattering_shape = tf.shape(S)[-2:]
-            new_shape = tf.concat((batch_shape, scattering_shape), 0)
+
+            scattering_shape = tf.shape(S)[1:]
+
+            S = tf.reshape(S, tf.concat((batch_shape, scattering_shape), 0))
 
             # S = tf.reshape(S, new_shape)
 